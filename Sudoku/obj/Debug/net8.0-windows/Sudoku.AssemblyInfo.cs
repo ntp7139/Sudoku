c25f138e14b0c9a8882ab11e9fa943b614fdbb50
Nguyen Chi <Chi.NT227088@sis.hust.edu.vn>
2024-12-17 10:37:31 +0700

    Luu

--- Sudoku/obj/Debug/net8.0-windows/Sudoku.AssemblyInfo.cs
+++ Sudoku/obj/Debug/net8.0-windows/Sudoku.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sudoku")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+451b6cd1243127dc9d7c6ce69b55a173441978f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sudoku")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sudoku")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
